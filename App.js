b61db12158e3a4df5d9fad096eb82c806f34d2a7
joey <jotosound@gmail.com>
2023-09-26 16:55:58 -0400

    Exercise 10.11: fetching repositories with Apollo Client

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NativeRouter } from 'react-router-native';
 
+import { ApolloProvider } from '@apollo/client';
+import createApolloClient from './src/utils/apolloClient';
 import Main from './src/components/Main';
+const apolloClient = createApolloClient()
 export default function App() {
+  console.log(Constants.manifest)
   return (
     <>
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
       <StatusBar style="auto" />
     </>
---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,11 @@
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  })
+  return { repositories: data?.repositories, loading, refetch: refetch };
+};
+
+export default useRepositories;
